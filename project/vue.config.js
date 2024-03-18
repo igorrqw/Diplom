20cc573f994b96f2fc5d0f9e99a48c2b51263a9e
Igor <warface.777@mail.ru>
2024-03-18 20:47:38 +0300

    test deploy

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: `/Diplom/project/`,
+  publicPath: `/Diplom/`,
   transpileDependencies: true,
 });
