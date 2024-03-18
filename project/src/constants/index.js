c154ad4dea1c7aaf19d511b320f830a5e30f347d
Igor <warface.777@mail.ru>
2024-03-18 20:18:16 +0300

    test diploy

--- project/src/constants/index.js
+++
@@ -1,1 +0,0 @@
-export const KP_API_KEY = process.env.VUE_APP_KP_API_KEY;
--- project/vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  publicPath: `/Diplom/project/`,
-  transpileDependencies: true,
-});
