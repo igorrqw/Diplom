530edaa5456faf46f2492f8bb69f28f99482113a
Igor <warface.777@mail.ru>
2024-03-18 18:19:38 +0300

    add gh-pages

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: `/project/`,
+  publicPath: `/Diplom/project/`,
   transpileDependencies: true,
 });
